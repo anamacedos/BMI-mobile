1b7b5509ba5650f836306c555756e8dd2176d56f
ANA JÚLIA SILVA MACEDO <24122496@dsn127m1241955.cfp127aluno.local>
2025-02-18 12:17:33 -0300

    tela inicial concluida

---
+++ app/src/main/java/br/senai/sp/jandira/bmi/screans/TelaInicial.kt
@@ -0,0 +1,138 @@
+package br.senai.sp.jandira.bmi.screans
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.packInts
+import br.senai.sp.jandira.bmi.R
+
+@Composable
+fun TelaInicial(modifier: Modifier = Modifier) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        Color(0xFF8C69D2),
+                        Color(0xFFFFFFFF)
+                    )
+                )
+            ), //antes de todas as cores tem que colocar 0xff
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Text(
+            text = "Olá Mundo!"
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.imagem_fitness),
+                contentDescription = stringResource(R.string.logo_description),
+                modifier = Modifier
+                    .padding(top = 35.dp)
+            )
+            Text(
+                text = stringResource(R.string.welcome),
+                fontSize = 37.sp,//sp é necessario import
+                color = Color.White
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(450.dp),
+                shape = RoundedCornerShape(
+                    topStart = 32.dp,
+                    topEnd = 32.dp
+                ) //forma de cantos arredondados
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.End,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp),
+
+                ) {
+                    Column {
+                        Text(
+                            text = stringResource(R.string.your_name),
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+
+                        )
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp),
+                            shape = RoundedCornerShape(12.dp),
+                            label = {
+                                Text(text = stringResource(R.string.your_name_here))
+                            }
+                        )
+                    }
+
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                    ) {
+                        Text(
+                            text = stringResource(R.string.next)
+                        )
+                        Icon(
+                            imageVector = Icons.Filled.ArrowForward,
+                            contentDescription = ""
+                        )
+                    }
+                }
+
+            }
+
+        }
+    }
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun TelaInicialPreview() {
+    TelaInicial()
+
+}
