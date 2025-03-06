fd743e2f51ff447cef82f8a2e29453d118c6db55
ANA JÚLIA SILVA MACEDO <24122496@dsn127m1241955.cfp127aluno.local>
2025-03-06 15:20:15 -0300

    salvando

--- app/src/main/java/br/senai/sp/jandira/bmi/screans/UserDataScream.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screans/UserDataScream.kt
@@ -2,13 +2,19 @@ package br.senai.sp.jandira.bmi.screans
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.senai.sp.jandira.bmi.R
 
 @Composable
@@ -19,9 +25,19 @@ fun UserDataScream(){
             .fillMaxSize()
             .background(color = Color(0xFF00897B))
     ){
-        Text(
-            text = stringResource(R.string.Next)
-        )
+//        Text(
+//            text = //stringResource(R.string.Next)
+//        )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(450.dp),
+            shape = RoundedCornerShape(
+                topStart =
+            )
+        ) {
+
+        }
     }
 
 }
